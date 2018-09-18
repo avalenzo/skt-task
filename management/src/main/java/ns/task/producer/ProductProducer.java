46ed6371990f8b85719290ac9b9d7e457f876cd1
Alejandra Valenzuela <avalenzuela@nearsoft.com>
2018-09-18 11:22:37 -0700

    Remove unused imports

--- management/src/main/java/ns/task/producer/ProductProducer.java
+++ management/src/main/java/ns/task/producer/ProductProducer.java
@@ -4,7 +4,6 @@ import ns.task.config.RabbitConfig;
 import ns.task.pojo.Product;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
